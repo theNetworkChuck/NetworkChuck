d31211a1e55353da58f448a8258fca856730aa31
theNetworkChuck <47433490+theNetworkChuck@users.noreply.github.com>
2019-09-25 00:20:07 -0500

    Update configlinuxlab.sh

--- configlinuxlab.sh
+++ configlinuxlab.sh
@@ -8,5 +8,5 @@ systemctl enable xrdp
 systemctl start xrdp
 
 yum groupinstall -y "xfce"
-echo "xfce4-session" > /home/linuxlab/.Xclients
-chmod a+x /home/linuxlab/.Xclients
+echo "xfce4-session" > ~/.Xclients
+chmod a+x ~/.Xclients
